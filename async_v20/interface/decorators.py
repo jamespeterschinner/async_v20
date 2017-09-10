46cb5dab9c672a61554726ab6e9e66c08b8d78de
James Schinner <james.peter.schinner@gmail.com>
2017-09-10 19:10:57 +1000

    Added code to endpoint decorator stub.

--- async_v20/interface/decorators.py
+++ async_v20/interface/decorators.py
@@ -1,3 +1,35 @@
-def endpoint(endpoint,):pass
+from .helpers import _create_body
+from .helpers import _parse_response
+from .helpers import _create_params
+from .helpers import _create_headers
+from .helpers import _create_path
+from functools import wraps
+
+def endpoint(endpoint):
+
+    header_args = endpoint.header_args()
+    query_args = endpoint.query_args()
+    
+    def wrapper(method):
+        @wraps(method)
+        async def wrap(self, *args, **kwargs):
+            arguments = args + tuple(kwargs.values())
+            json_body = await _create_body(self, endpoint.request_schema, arguments)
+            headers = await _create_headers(self, header_args, arguments)
+            path = await _create_path(self, endpoint, endpoint.path, arguments)
+            parameters = await _create_params(self, query_args, arguments)
+
+            # TODO add json data to request do iu need to await this?
+            response = self.session.request(method=endpoint.method,
+                                            path=path,
+                                            headers=headers,
+                                            parameters=parameters)
+
+            return await _parse_response(self, response, endpoint)
+        return wrap
+    return wrapper
+
+
+
 
 def add_signature(callable):pass
