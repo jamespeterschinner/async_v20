6eb3f6ce026b92746d34a7c868b4b5b05297e948
James Schinner <james.peter.schinner@gmail.com>
2017-09-18 15:03:23 +1000

    Added __init__ to definitions

---
+++ async_v20/definitions/__init__.py
@@ -0,0 +1,4 @@
+from .types import *
+from .descriptors import *
+
+__all__ = (types.__all__ + descriptors.__all__)
--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='async_v20',
       version='0.1',
       description="Asynchronous wrapper for OANDA's v20 REST API",
-      url='',
+      url='N/A',
       author='James Peter Schinner',
-      author_email='james.peter.schinner@gnail.com',
+      author_email='james.peter.schinner@gmail.com',
       license='MIT',
-      packages=['async_v20'],
-      zip_safe=False)
+      packages=find_packages(),
+      install_requires=['aiohttp>=2.2.5',
+                        'pytest>=3.2.2',
+                        'ujson==1.35',
+                        'yarl==0.12.0']
+)
