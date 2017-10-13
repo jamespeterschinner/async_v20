4865f14d18b0bb7b320e606b03e20e08869acc9a
James Schinner <james.peter.schinner@gmail.com>
2017-10-14 09:38:22 +1100

    Model.json_dict() now accepts 'float_to_string' parameter. Required to correctly convert objects to pandas.Series or to serialize objects ready to send to OANDA

--- async_v20/definitions/base.py
+++ async_v20/definitions/base.py
@@ -31,26 +31,27 @@ class Model(metaclass=ORM):
         return self.__class__.__name__
 
     # Recursion might be an issue
-    def json_dict(self):
+    def json_dict(self, float_to_string=True):
         def get_object_fields(attr):
             attr = getattr(self, attr)
             if not isinstance(attr, (int, float, str)):
                 try:
-                    attr = attr.json_dict()
+                    attr = attr.json_dict(float_to_string)
                 except AttributeError:
-                    attr = [obj.json_dict() for obj in attr]
-            elif isinstance(attr, float):
+                    attr = [obj.json_dict(float_to_string) for obj in attr]
+            elif float_to_string and isinstance(attr, float):
                 attr = str(attr)
             return attr
 
         return {self.__class__.json_attributes[attr]: get_object_fields(attr)
                 for attr in self._fields}
 
+
     def json_data(self):
         return json.dumps({self.json_dict()})
 
-    def data(self):
-        return flatten_dict(self.json_dict(), self._delimiter)
+    def data(self, float_to_string=True):
+        return flatten_dict(self.json_dict(float_to_string), self._delimiter)
 
     def series(self):
-        return pd.Series(dict(self.template, **self.data()))
+        return pd.Series(dict(self.template, **self.data(float_to_string=False)))
--- tests/test_definitions/test_base.py
+++ tests/test_definitions/test_base.py
@@ -1,12 +1,17 @@
 import pytest
-from ..data.json_data import GETAccountID_response
-from async_v20.definitions.types import Account
 from async_v20.definitions.base import Model
+from async_v20.definitions.types import Account
+from async_v20.definitions.helpers import flatten_dict
+
+from ..data.json_data import GETAccountID_response
+
+
 @pytest.fixture
 def account():
     result = Account(**GETAccountID_response['account'])
     yield result
     del result
+
 
 def test_account_has_correct_methods(account):
     assert hasattr(account, 'json_dict')
@@ -20,10 +25,12 @@ def test_kwargs():
     yield kwargs
     del kwargs
 
+
 @pytest.fixture
 def test_class():
     class TestClass(Model):
         _dispatch = {'type': 'LIST'}
+
     test_cls = TestClass
     yield test_cls
     del test_cls
@@ -31,3 +38,29 @@ def test_class():
 
 def test_base_dispatch_works_correctly():
     pass
+
+
+def test_json_dict_returns_correct_data_structure(account):
+    """Test the result is formatted correctly. There is a requirement for
+    json_dict to be able to cast floats to strings, this is necessary when
+    serializing objects to send to OANDA. Though when used internally is it more
+    natural to leave floats as floats."""
+
+    result = account.json_dict(float_to_string=True)
+    # Test result is a dict
+    assert type(result) == dict
+    flattened_result = flatten_dict(result)
+    # Test that all values have the correct data type
+    for value in flattened_result:
+        assert isinstance(value, (dict, str, int, list))
+
+    result = account.json_dict(float_to_string=False)
+    assert type(result) == dict
+    flattened_result = flatten_dict(result)
+    # Test that all values have the correct data type. Specifically that
+    # all floats have not been casted to a string.
+    for value in flattened_result:
+        assert isinstance(value, (dict, float, str, int, list))
+        if isinstance(value, str):
+            with pytest.raises(ValueError):
+                float(value)
