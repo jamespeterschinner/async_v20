d92b675986379f05c4ef65702cf966487b600e8a
James Schinner <james.peter.schinner@gmail.com>
2017-10-13 17:57:45 +1100

    serializing objects is no longer async

--- bin/candles.py
+++ bin/candles.py
@@ -6,12 +6,11 @@ from async_v20.client import OandaClient
 async def candles():
     client = OandaClient()
     try:
-        candles_1 = await client.get_candles('AUD_USD')
-        candles_2 = await client.get_candles('EUR_USD')
+        response = await client.get_candles('AUD_USD')
     finally:
         client.session.close()
-    return candles_1, candles_2
+    return response
 
 
 loop = asyncio.get_event_loop()
-candle_data = loop.run_until_complete(candles())
+response = loop.run_until_complete(candles())
--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(name='async_v20',
-      version='0.1',
+      version='1.0.0a1',
       description="Asynchronous wrapper for OANDA's v20 REST API",
-      url='N/A',
+      long_description="Asynchronous wrapper for OANDA's v20 REST API. Client methods return coroutines that can be run"
+                       "inside an asyncio event loop",
       author='James Peter Schinner',
       author_email='james.peter.schinner@gmail.com',
+      url='https://github.com/jamespeterschinner/async_v20',
       license='MIT',
       packages=find_packages(),
       install_requires=['aiohttp>=2.2.5',
@@ -13,5 +15,12 @@ setup(name='async_v20',
                         'ujson==1.35',
                         'yarl==0.12.0',
                         'inflection==0.3.1',
-                        'pandas==0.20.3']
-)
+                        'pandas==0.20.3'],
+      classifiers=['Programming Language :: Python :: 3.6', 'Development Status :: 3 - Alpha',
+                   'Framework :: AsyncIO',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: MIT License',
+                   'Operating System :: OS Independent',
+                   'Topic :: Internet :: WWW/HTTP :: Session'],
+      keywords='algorithmic trading oanda v20 REST asyncio ',
+      )
