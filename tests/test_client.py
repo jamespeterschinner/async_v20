a44b890ba68a5fee9e39f782ca0bbd2081a5c409
James Schinner <james.peter.schinner@gmail.com>
2017-10-24 21:56:07 +1100

    Changed slash direction

--- tests/test_client.py
+++ tests/test_client.py
@@ -4,14 +4,6 @@ import os
 import re
 from async_v20.endpoints.annotations import Authorization
 
-@pytest.fixture()
-def setup():
-    try:
-        with open(os.path.abspath(r'../../async_v20/setup.py'), 'r') as f:
-            yield f.read()
-    except FileNotFoundError:
-        with open(os.path.abspath(r'../jamespeterschinner/async_v20/setup.py'), 'r') as f:
-            yield f.read()
 
 @pytest.fixture()
 def client():
@@ -19,9 +11,15 @@ def client():
     yield oanda_client
     del oanda_client
 
-def test_oanda_client_has_correct_version(client, setup):
-    setup_version = re.findall(r"(?<=version\s=\s').*?(?='\n)", setup)[0]
-    assert setup_version == client.version
+def test_oanda_client_has_correct_version(client):
+    try:
+        with open(os.path.abspath(r'../../async_v20/setup.py'), 'r') as f:
+            setup = f.read()
+    except FileNotFoundError as e:
+        print(e)
+    else:
+        setup_version = re.findall(r"(?<=version\s=\s').*?(?='\n)", setup)[0]
+        assert setup_version == client.version
 
 def test_oanda_client_finds_token():
     os.environ['OANDA_TOKEN'] = 'test_environ_token'
