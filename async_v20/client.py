1d00bdf328c9a8673dea7ef0d95c726cc3b2055c
James Schinner <james.peter.schinner@gmail.com>
2017-10-25 15:07:41 +1100

    Update Version Update CHANGELOG

--- async_v20/client.py
+++ async_v20/client.py
@@ -8,7 +8,7 @@ from .endpoints.annotations import Authorization
 from .helpers import request_limiter, initializer
 from .interface import *
 
-version = '1.1.2a5'
+version = '1.1.3a0'
 
 
 class OandaClient(AccountInterface, InstrumentInterface, OrderInterface, PositionInterface,
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 
 from setuptools import setup, find_packages
-version = '1.1.2a5'
+version = '1.1.3a0'
 
 def read(f):
     return open(os.path.join(os.path.dirname(__file__), f)).read().strip()
