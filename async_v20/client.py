39e678ac59e1c2419e9e73781f3034c1427c2b12
James Schinner <james.peter.schinner@gmail.com>
2017-12-05 15:45:40 +1100

    Added attribute. half_spread_cost. To:
    - Transaction
    - TradeOpen
    - TradeReduce
    
    Added test & version bump

--- async_v20/client.py
+++ async_v20/client.py
@@ -20,7 +20,7 @@ async def sleep(s=0.0):
     await asyncio.sleep(s)
 
 
-__version__ = '2.2.4b3'
+__version__ = '2.2.4b4'
 
 
 class OandaClient(AccountInterface, InstrumentInterface, OrderInterface, PositionInterface,
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 
 from setuptools import setup, find_packages
 
-version = '2.2.4b3'
+version = '2.2.4b4'
 
 
 def read(f):
