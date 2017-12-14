6d7176bbfcb9cd75975d2cb495031f405be781f7
James Schinner <james.peter.schinner@gmail.com>
2017-12-14 19:58:09 +1100

    Added MANIFEST.in

--- async_v20/client.py
+++ async_v20/client.py
@@ -20,7 +20,7 @@ async def sleep(s=0.0):
     await asyncio.sleep(s)
 
 
-__version__ = '3.0.0b0'
+__version__ = '3.0.0b1'
 
 
 class OandaClient(AccountInterface, InstrumentInterface, OrderInterface, PositionInterface,
