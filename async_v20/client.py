9cb788764ed6da28bd9a2348f3973dd0c263f07c
James Schinner <james.peter.schinner@gmail.com>
2017-12-21 13:00:58 +1100

    Bump version

--- async_v20/client.py
+++ async_v20/client.py
@@ -20,7 +20,7 @@ async def sleep(s=0.0):
     await asyncio.sleep(s)
 
 
-__version__ = '4.0.0b0'
+__version__ = '5.0.0b0'
 
 
 class OandaClient(AccountInterface, InstrumentInterface, OrderInterface, PositionInterface,
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 
 from setuptools import setup, find_packages
 
-version = '4.0.0b0'
+version = '5.0.0b0'
 
 
 def read(f):
